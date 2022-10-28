6388159d9b9cc6c130f9cd0a0712dd4e70f989c3
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-28 11:33:11 +0530

    Code for Anagram Detection

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
+            string str1 = "heart";
+            string str2 = "earth";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string s1 = new string(ch1);
+            string s2 = new string(ch2);
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Anagram");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
         }
     }
 }
